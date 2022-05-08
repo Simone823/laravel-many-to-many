c5afe0ffd74af1f3e9beb0baada8cefa88f1bfc8
Simone823 <simone.daglio823@outlook.it>
2022-05-09 00:19:25 +0200

    fillable tag model

--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    // funzione fillable
+    protected $fillable =  [
+        'name'
+    ];
+
     // Funzione relazione tabella posts
     public function posts() {
         return $this->belongsToMany('App\Post');
