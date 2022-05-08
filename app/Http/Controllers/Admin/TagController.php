50d1c5835082f09ba9eed1ea6b616ca62760fca4
Simone823 <simone.daglio823@outlook.it>
2022-05-09 00:19:53 +0200

    tag controller create store

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TagController extends Controller
 {
@@ -29,7 +30,8 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        // Return view admin tags 
+        return view('admin.tags.create');
     }
 
     /**
@@ -40,7 +42,48 @@ class TagController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        // Recupero la richiesta
+        $data = $request->all();
+
+        // Validazione dati request
+        $request->validate([
+            'name' => 'required|max:150'
+        ]);
+
+        // Slug tags name
+        $slug = Str::slug($data['name']);
+
+        // Slug base
+        $slug_base = $slug;
+
+        // Counter slug
+        $counter = 1;
+
+        // Tags present
+        $tags_present = Tag::where('slug', $slug)->first();
+
+        // While Tags present
+        while ($tags_present) {
+            $slug = $slug_base . '-' . $counter;
+            $counter++;
+            $tags_present = Tag::where('slug', $slug)->first();
+        }
+
+        // Creo un nuovo tag
+        $tag = new Tag();
+
+        // Recupero i dati con il metodo fill
+        $tag->fill($data);
+
+        // Imposto lo slug
+        $tag->slug = $slug;
+
+        // Salvo i dati
+        $tag->save();
+
+        // return redirect admin tags index
+        return redirect()->route('admin.tags.index');
     }
 
     /**
