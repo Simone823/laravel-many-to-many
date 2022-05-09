9425e87605ec5e8dab013427d13a1669be23339b
Simone823 <simone.daglio823@outlook.it>
2022-05-09 13:18:33 +0200

    modifica table admin tags index

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -5,52 +5,57 @@
 @section('content')
 
     {{-- Sezione tabella tags --}}
-    <section id="section_table_tags">
+    <section id="section_table_tags" class="container">
 
         {{-- Title --}}
         <h3 class="text-white">Tags</h3>
 
         {{-- Table tags --}}
-        <table class="table_categories">
+        <table class="table table-striped table-secondary">
 
             {{-- Table header --}}
-            <tr>
-                <th>id</th>
-                <th>Name</th>
-                <th>Slug</th>
-                <th>Created_at</th>
-                <th>Updated_at</th>
-                <th></th>
-                <th></th>
-            </tr>
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Created_at</th>
+                    <th scope="col">Updated_at</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
 
-            {{-- Foreach tags --}}
-            @foreach ($tags as $element)
-            <tr>
-                <td>{{$element->id}}</td>
-                <td>{{$element->name}}</td>
-                <td>{{$element->slug}}</td>
-                <td>{{$element->created_at}}</td>
-                <td>{{$element->updated_at}}</td>
-                <td>
-                    <a class="btn btn-warning" href="{{route('admin.tags.edit', $element)}}">Modifica</a>
-                </td>
-                <td>
-                    <form action="{{route('admin.tags.destroy', $element)}}" method="POST">
-
-                        {{-- Key --}}
-                        @csrf
-
-                        {{-- Method delete --}}
-                        @method('DELETE')
-
-                        {{-- Button delete --}}
-                        <button class="btn btn-danger" type="submit">Elimina</button>
-
-                    </form>
-                </td>
-            </tr>
-            @endforeach
+            {{-- Table body --}}
+            <tbody>
+                {{-- Foreach tags --}}
+                @foreach ($tags as $element)
+                    <tr>
+                        <td>{{$element->id}}</td>
+                        <td>{{$element->name}}</td>
+                        <td>{{$element->slug}}</td>
+                        <td>{{$element->created_at}}</td>
+                        <td>{{$element->updated_at}}</td>
+                        <td>
+                            <a class="btn btn-warning" href="{{route('admin.tags.edit', $element)}}">Modifica</a>
+                        </td>
+                        <td>
+                            <form action="{{route('admin.tags.destroy', $element)}}" method="POST">
+        
+                                {{-- Key --}}
+                                @csrf
+        
+                                {{-- Method delete --}}
+                                @method('DELETE')
+        
+                                {{-- Button delete --}}
+                                <button class="btn btn-danger" type="submit">Elimina</button>
+        
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>   
 
         </table>
 
