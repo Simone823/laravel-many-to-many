cd831f312edc6e866da553d65f2107b6f9d6e0e9
Simone823 <simone.daglio823@outlook.it>
2022-05-09 13:17:20 +0200

    modifca table admin homepage

--- resources/views/admin/homepage.blade.php
+++ resources/views/admin/homepage.blade.php
@@ -11,33 +11,38 @@
         <h3 class="text-white">Users</h3>
 
         {{-- Table users --}}
-        <table class="table_users">
+        <table class="table table-striped table-secondary">
 
             {{-- Table header --}}
-            <tr>
-                <th>id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Email_verified_at</th>
-                <th>Password</th>
-                <th>Remember_token</th>
-                <th>Created_at</th>
-                <th>Updated_at</th>
-            </tr>
-
-            {{-- Foreach users --}}
-            @foreach ($users as $element)
+            <thead>
                 <tr>
-                    <td>{{$element->id}}</td>
-                    <td>{{$element->name}}</td>
-                    <td>{{$element->email}}</td>
-                    <td>{{$element->email_verified_at == null ? 'null' : $element->email_verified_at}}</td>
-                    <td>{{$element->password}}</td>
-                    <td>{{$element->remember_token == null ? 'null' : $element->remember_token}}</td>
-                    <td>{{$element->created_at}}</td>
-                    <td>{{$element->updated_at}}</td>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Email_verified_at</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Remember_token</th>
+                    <th scope="col">Created_at</th>
+                    <th scope="col">Updated_at</th>
                 </tr>
-            @endforeach
+            </thead>
+
+            {{-- Table body --}}
+            <tbody>
+                {{-- Foreach users --}}
+                @foreach ($users as $element)
+                    <tr>
+                        <td>{{$element->id}}</td>
+                        <td>{{$element->name}}</td>
+                        <td>{{$element->email}}</td>
+                        <td>{{$element->email_verified_at == null ? 'null' : $element->email_verified_at}}</td>
+                        <td>{{$element->password}}</td>
+                        <td>{{$element->remember_token == null ? 'null' : $element->remember_token}}</td>
+                        <td>{{$element->created_at}}</td>
+                        <td>{{$element->updated_at}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
 
         </table>
 
@@ -50,27 +55,32 @@
         <h3 class="text-white">Categories</h3>
 
         {{-- Table categories --}}
-        <table class="table_categories">
+        <table class="table table-striped table-secondary">
 
             {{-- Table header --}}
-            <tr>
-                <th>id</th>
-                <th>Name</th>
-                <th>Slug</th>
-                <th>Created_at</th>
-                <th>Updated_at</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Created_at</th>
+                    <th scope="col">Updated_at</th>
+                </tr>
+            </thead>
 
-            {{-- Foreach categories --}}
-            @foreach ($categories as $element)
-            <tr>
-                <td>{{$element->id}}</td>
-                <td>{{$element->name}}</td>
-                <td>{{$element->slug}}</td>
-                <td>{{$element->created_at}}</td>
-                <td>{{$element->updated_at}}</td>
-            </tr>
-            @endforeach
+            {{-- Table body --}}
+            <tbody>
+                {{-- Foreach categories --}}
+                @foreach ($categories as $element)
+                <tr>
+                    <td>{{$element->id}}</td>
+                    <td>{{$element->name}}</td>
+                    <td>{{$element->slug}}</td>
+                    <td>{{$element->created_at}}</td>
+                    <td>{{$element->updated_at}}</td>
+                </tr>
+                @endforeach
+            </tbody>
 
         </table>
 
@@ -82,30 +92,35 @@
         {{-- Title --}}
         <h3 class="text-white">Tags</h3>
 
-        {{-- Table categories --}}
-        <table class="table_tags">
+        {{-- Table tags --}}
+        <table class="table table-striped table-secondary">
 
             {{-- Table header --}}
-            <tr>
-                <th>id</th>
-                <th>Name</th>
-                <th>Slug</th>
-                <th>Created_at</th>
-                <th>Updated_at</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Created_at</th>
+                    <th scope="col">Updated_at</th>
+                </tr>
+            </thead>
 
-            {{-- Foreach categories --}}
-            @foreach ($tags as $element)
-            <tr>
-                <td>{{$element->id}}</td>
-                <td>
-                    <span style="background-color: {{$element->color}}; font-size: 14px;" class="badge badge-pill py-2 px-3 my-2 text-dark">{{$element->name}}</span>
-                </td>
-                <td>{{$element->slug}}</td>
-                <td>{{$element->created_at}}</td>
-                <td>{{$element->updated_at}}</td>
-            </tr>
-            @endforeach
+            {{-- Table body --}}
+            <tbody>
+                {{-- Foreach categories --}}
+                @foreach ($tags as $element)
+                <tr>
+                    <td>{{$element->id}}</td>
+                    <td>
+                        <span style="background-color: {{$element->color}}; font-size: 14px;" class="badge badge-pill py-2 px-3 my-2 text-dark">{{$element->name}}</span>
+                    </td>
+                    <td>{{$element->slug}}</td>
+                    <td>{{$element->created_at}}</td>
+                    <td>{{$element->updated_at}}</td>
+                </tr>
+                @endforeach
+            </tbody>
 
         </table>
 
@@ -119,54 +134,58 @@
         <h3 class="text-white">Posts</h3>
 
         {{-- Table posts --}}
-        <table class="table_posts">
+        <table class="table table-striped table-secondary">
 
             {{-- Table header --}}
-            <tr>
-                <th>id</th>
-                <th>Title</th>
-                <th>Slug</th>
-                <th>User</th>
-                <th>Description</th>
-                <th>Category_id</th>
-                <th>Tags</th>
-                <th>Image</th>
-                <th>Publication_date</th>
-                <th>Created_at</th>
-                <th>Updated_at</th>
-            </tr>
-
-            {{-- Foreach posts --}}
-            @foreach ($posts as $element)
+            <thead>
                 <tr>
-                    <td>{{$element->id}}</td>
-                    <td>{{$element->title}}</td>
-                    <td>{{$element->slug}}</td>
-                    <td>
-                        <span class="badge badge-pill badge-dark text-white font-weight-bold px-3 py-2">{{$element->user->name}}</span>
-                    </td>
-
-                    <td>{{$element->description}}</td>
-                    <td>{{$element->category ? $element->category->name : 'null'}}</td>
-                    <td>
-                        @foreach ($element->tags as $tag)
-                            <span style="background-color: {{$tag->color}}; font-size: 14px;" class="badge badge-pill py-2 px-3 my-2 text-dark">{{$tag->name}}</span>
-                        @endforeach
-                    </td>
-                    <td>
-                        <figure class="img_wrapper">
-                            <img src="{{$element->image}}" alt="">
-                        </figure>
-                    </td>
-                    <td>{{$element->publication_date == null ? 'null' : $element->publication_date}}</td>
-                    <td>{{$element->created_at}}</td>
-                    <td>{{$element->updated_at}}</td>
+                    <th scope="col">id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Category_id</th>
+                    <th scope="col">Tags</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Publication_date</th>
+                    <th scope="col">Created_at</th>
+                    <th scope="col">Updated_at</th>
                 </tr>
-            @endforeach
+            </thead>
+
+            {{-- Table body --}}
+            <tbody>
+                {{-- Foreach posts --}}
+                @foreach ($posts as $element)
+                    <tr>
+                        <td>{{$element->id}}</td>
+                        <td>{{$element->title}}</td>
+                        <td>{{$element->slug}}</td>
+                        <td>
+                            <span class="badge badge-pill badge-dark text-white font-weight-bold px-3 py-2">{{$element->user->name}}</span>
+                        </td>
+    
+                        <td>{{$element->description}}</td>
+                        <td>{{$element->category ? $element->category->name : 'null'}}</td>
+                        <td>
+                            @foreach ($element->tags as $tag)
+                                <span style="background-color: {{$tag->color}}; font-size: 14px;" class="badge badge-pill py-2 px-3 my-2 text-dark">{{$tag->name}}</span>
+                            @endforeach
+                        </td>
+                        <td>
+                            <figure class="img_wrapper">
+                                <img src="{{$element->image}}" alt="">
+                            </figure>
+                        </td>
+                        <td>{{$element->publication_date == null ? 'null' : $element->publication_date}}</td>
+                        <td>{{$element->created_at}}</td>
+                        <td>{{$element->updated_at}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
 
         </table>
 
     </section>
-
 
 @endsection
